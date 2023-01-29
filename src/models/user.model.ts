523dfbd4435785e03b826c6855fb4767f75907eb
rafaelBporto <rafaelbporto88@gmail.com>
2023-01-29 20:48:13 -0300

    refact: model user

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,6 +1,7 @@
+import { Prisma } from "@prisma/client";
 import Joi from "joi";
 
-const userSchema = Joi.object({
+const userSchema = Joi.object<Prisma.usersCreateInput>({
     name: Joi.string().trim().required()
 })
 
